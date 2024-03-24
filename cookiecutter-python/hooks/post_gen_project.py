c907672f0fff39bc890e751d2b65292e81030ac1
mohankumargupta <mohankumargupta@users.noreply.github.com>
2024-03-25 10:37:24 +1100

    untested python cli

--- cookiecutter-python/hooks/post_gen_project.py
+++ cookiecutter-python/hooks/post_gen_project.py
@@ -1,3 +1,7 @@
 from pathlib import Path
+import shutil
+
 if "{{ cookiecutter.project_template }}" == "cli":
-    Path('__init__.py').touch()
+    Path("{{ cookiecutter.project_name }}").mkdir()
+    Path("{{ cookiecutter.project_name }}").joinpath('__init__.py').touch()
+    shutil.move("main.py", "{{ cookiecutter.project_name }}")
