49b298b595019d2d2418e42f73922f4b5ddb759b
mohankumargupta <mohankumargupta@users.noreply.github.com>
2024-04-07 15:17:52 +1000

    python_cli: use proper python file structure

--- cookiecutter-python/hooks/post_gen_project.py
+++ cookiecutter-python/hooks/post_gen_project.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import shutil
 
 if "{{ cookiecutter.project_template }}" == "cli":
-    Path("{{ cookiecutter.project_name }}").mkdir()
-    Path("{{ cookiecutter.project_name }}").joinpath('__init__.py').touch()
-    shutil.move("main.py", "{{ cookiecutter.project_name }}")
+    src = Path("src")
+    src.mkdir()
+    project = src.joinpath("{{ cookiecutter.project_name }}")
+    project.mkdir()
+    project.joinpath('__init__.py').touch()
+    shutil.move("main.py", project)
