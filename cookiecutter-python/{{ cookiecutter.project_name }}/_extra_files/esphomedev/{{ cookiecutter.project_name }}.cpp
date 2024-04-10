f6b9888e66a12c6b8348cc98d50da1909fcb2162
mohankumargupta <mohankumargupta@users.noreply.github.com>
2024-04-11 09:01:45 +1000

    extra files

--- cookiecutter-python/{{ cookiecutter.project_name }}/_extra_files/esphomedev/{{ cookiecutter.project_name }}.cpp
+++ cookiecutter-python/{{ cookiecutter.project_name }}/_extra_files/esphomedev/{{ cookiecutter.project_name }}.cpp
@@ -3,20 +3,20 @@
 namespace esphome {
 namespace {{ cookiecutter.project_name }} {
 
-static const char *TAG = "empty_component.component";
+static const char *TAG = "{{ cookiecutter.project_name }}.component";
 
-void {{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }})Component::setup() {
-
-}
-
-void EmptyComp{{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }})Componentonent::loop() {
+void {{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }}Component::setup() {
 
 }
 
-void {{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }})Component::dump_config(){
+void EmptyComp{{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }}Componentonent::loop() {
+
+}
+
+void {{ cookiecutter.project_name[0]|upper }}{{ cookiecutter.project_name[1:] }}Component::dump_config(){
     ESP_LOGCONFIG(TAG, "Empty component");
 }
 
 
-}  // namespace empty_component
+}  // namespace {{ cookiecutter.project_name }}
 }  // namespace esphome
