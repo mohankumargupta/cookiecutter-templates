3f089ebbb5ef6faad47354ca6efba6e99b991d2b
Mohan Gupta <mohangupta@live.com>
2024-04-28 14:56:11 +1000

    arduino: would be nice

--- cookiecutter-arduino/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}.ino
+++ cookiecutter-arduino/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}.ino
@@ -1,1 +1,11 @@
-{%- include "{{ cookiecutter.arduino }}/sketch.ino" %}
+{%- if cookiecutter.arduino == "esp32s2" %}
+{%- include "esp32s2/sketch.ino" %}
+{%- elif cookiecutter.arduino == "esp32" %}
+{%- include "esp32/sketch.ino" %}
+{%- if cookiecutter.arduino == "esp32c3" %}
+{%- include "esp32c3/sketch.ino" %}
+{%- elif cookiecutter.arduino == "esp32s3" %}
+{%- include "esp32s3/sketch.ino" %}
+{%- elif cookiecutter.arduino == "nano" %}
+{%- include "nano/sketch.ino" %}
+{% endif %}
