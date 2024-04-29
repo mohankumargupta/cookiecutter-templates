f056b30cfb0d6cd90a87ace281cb213df5c8ff26
Mohan Gupta <mohangupta@live.com>
2024-04-29 12:10:17 +1000

    yep

--- cookiecutter-wokwi/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}.ino
+++ cookiecutter-wokwi/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}.ino
@@ -1,11 +1,2 @@
-{%- if cookiecutter.arduino == "esp32s2" %}
-{%- include "esp32s2/sketch.ino" %}
-{%- elif cookiecutter.arduino == "esp32" %}
-{%- include "esp32/sketch.ino" %}
-{%- elif cookiecutter.arduino == "esp32c3supermini" %}
-{%- include "esp32c3/sketch.ino" %}
-{%- elif cookiecutter.arduino == "esp32s3" %}
-{%- include "esp32s3/sketch.ino" %}
-{%- elif cookiecutter.arduino == "nano" %}
-{%- include "nano/sketch.ino" %}
-{% endif %}
+{% set sketch = cookiecutter.project_name + "/sketch.ino" %}
+{% include sketch %}
